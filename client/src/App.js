dd97bb3792211856e09ef7ebaabd59eede25fce9
Atharva Ansingkar <atharva@gmail.com>
2024-08-05 00:06:49 +0530

    chat widget integration in react

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import Chatbot from './components/Chatbot';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chatbot</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
