7a155bfd4154913ad0a6b2f038e25e3a543f4080
atharvaansingkar <atharvaansingkar2004@gmail.com>
2024-10-21 23:33:27 +0530

    frontend update

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,23 @@ import Chatbot from './components/Chatbot'
 function App() {
   return (
     <div className="App">
+    {/* Embed the dashboard.html inside an iframe */}
+    <iframe
+      src="/EduPlusCampus.html"
+      title="Dashboard"
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        border: 'none',
+      }}
+    />
+
+    <div className="App">
       <Chatbot />
+    </div>
     </div>
   );
 }
--- client/src/components/Chatbot.js
+++ client/src/components/Chatbot.js
@@ -13,6 +13,7 @@ class Chatbot extends React.Component {
   handleNewUserMessage = async (newMessage) => {
     toggleMsgLoader(); // Start loader
 
+
     try {
       // Send user message to Rasa backend
       const response = await axios.post('http://localhost:5005/webhooks/rest/webhook', {
